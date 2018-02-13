b0f2db316086dbc777a4cf8c599dc78602a64fe8
TheBrooks <the.ryan.brooks@gmail.com>
2018-02-13 13:51:37 -0700

    figuring out how to load index.html

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 express()
-  //.use(express.static(path.join(__dirname, "css")))
-  .get("/", (req, res) => res.render("index"))
+  .get("/", (req, res) => res.send("index.html"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
